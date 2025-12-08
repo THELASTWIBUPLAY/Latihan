d8015ca319d3691713373d3488f14a141cd57cd4
Muhamad Masyhuri <masyhuri161003@gmail.com>
2025-12-08 10:08:21 +0700

    untested show data

--- app/src/main/java/com/example/latihan/MainActivity.kt
+++ app/src/main/java/com/example/latihan/MainActivity.kt
@@ -2,26 +2,36 @@ package com.example.latihan
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.latihan.CreateCatatan
+import com.example.latihan.R
+import com.example.latihan.RetrofitClient
 import com.example.latihan.databinding.ActivityMainBinding
+import com.example.latihan.adapter.CatatanAdapter
+import com.example.latihan.repository.CatatanRepository
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: ActivityMainBinding
+    private lateinit var adapter: CatatanAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.updatePadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
@@ -29,9 +39,40 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun setupEvents() {
+        adapter = CatatanAdapter(mutableListOf())
+        binding.container.adapter = adapter
+        binding.container.layoutManager = LinearLayoutManager(this)
+
         binding.btnNavigateCreate.setOnClickListener {
             val intent = Intent(this, CreateCatatan::class.java)
             startActivity(intent)
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        loadData()
+    }
+
+    fun loadData() {
+        lifecycleScope.launch {
+            val response = RetrofitClient.catatanRepository.getCatatan()
+            if (!response.isSuccessful) {
+                displayMessage("Gagal : ${response.message()}")
+                return@launch
+            }
+
+            val data = response.body()
+            if (data == null) {
+                displayMessage("Tidak ada data")
+                return@launch
+            }
+
+            adapter.updateData(data)
+        }
+    }
+
+    fun displayMessage(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
 }
